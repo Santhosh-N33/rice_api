2e961758fe215aa55b2535384c07e9832e82454c
Santhosh N <129830790+Santhosh-N33@users.noreply.github.com>
2025-05-03 21:34:32 +0530

    Update app.py modify app

--- app.py
+++ app.py
@@ -55,5 +55,5 @@ def predict():
         print(f"❌ Internal error during prediction: {e}")
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5002)
